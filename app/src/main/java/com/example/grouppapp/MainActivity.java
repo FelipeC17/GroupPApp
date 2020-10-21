4bfdfb9b0bedb06755a83a0149d6c3f74d1e3dc7
FelipeC17 <1841029@brunel.ac.uk>
2020-10-21 19:19:24 +0100

    Deleted code due to errors that were not fixed

--- app/src/main/java/com/example/grouppapp/MainActivity.java
+++ app/src/main/java/com/example/grouppapp/MainActivity.java
@@ -17,65 +17,11 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText passL, emaiL;
-    Button bttnL;
-    FirebaseAuth Fiauth;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
-        emaiL = findViewById(R.id.EmailL);
-        passL = findViewById(R.id.PasswordL);
-        bttnL = findViewById(R.id.buttonL);
-        Fiauth = FirebaseAuth.getInstance();
-
-        bttnL.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String emailL = emaiL.getText().toString().trim();
-                String passwordL = passL.getText().toString().trim();
-
-
-                if (TextUtils.isEmpty(emailL)) {
-                    emaiL.setError("Email is Required");
-                    return;
-                }
-
-                if (TextUtils.isEmpty(passwordL)) {
-                    passL.setError("Password is Required");
-                    return;
-                }
-
-                if (passwordL.length() < 6) {
-                    passL.setError("Password must contain 6 or more characters");
-                    return;
-                }
-
-
-                Fiauth.signInWithEmailAndPassword(emailL,passwordL).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-
-                        if (task.isSuccessful()){
-                            Toast.makeText(MainActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
-                        }
-                        else{
-                            Toast.makeText(MainActivity.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-
-
-                    }
-                });
-
-            }
-        });
-
-
 
 
 
---
+++ app/src/main/java/com/example/grouppapp/UserProfile.java
@@ -0,0 +1,48 @@
+package com.example.grouppapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class UserProfile extends AppCompatActivity {
+
+    Button LogOut;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+
+        LogOut = findViewById(R.id.LOut);
+
+        LogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(UserProfile.this, MainActivity.class));
+
+            }
+        });
+
+
+    }
+}
